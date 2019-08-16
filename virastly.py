cca082512e34957d6b6cda725fd27a05f94adb2e
Amir Latifi <amiirlatifi@gmail.com>
2019-08-17 00:05:37 +0430

    Reading file contents.

--- virastly.py
+++ virastly.py
@@ -2,13 +2,33 @@ import sys
 
 
 def viraiesh(text):
-    return text.replace('ي', 'ی')
+    text = text.replace('ي', 'ی')
+    text = text.replace('ك', 'ک')
+    text = text.replace('ـ', '')
+    text = text.replace(' می ', ' می‌')
+    text = text.replace(' نمی ', ' نمی‌‌')
+    text = text.replace(' هـای ', '‌هـای ')
+    text = text.replace('میتوان', 'می‌توان')
+    text = text.replace('میكنم', 'می‌كنم')
+    text = text.replace('میدهن', 'می‌‌دهن')
+    text = text.replace('میكند', 'می‌كند')
+    text = text.replace('نمیگذارد', 'نمی‌گذارد')
+    text = text.replace('میگوید', 'می‌گوید')
+    text = text.replace(' تـرين ', ' تـرين‌')
+    text = text.replace(' تـری ', ' تـری‌')
+    return text.replace('ة', 'ه‌ی')
 
 
-def execute():
-    text = str(sys.argv[1])
-    result = viraiesh(text)
-    print(result)
+def main():
+    file = open(sys.argv[1], "r")
+    if file.mode != 'r':
+        print(sys.argv[1] + ' is not readable')
+    else:
+        text = file.read()
+        result = viraiesh(text)
+        f = open("result.txt", "w+")
+        f.write(result)
+        print('Done! Check result.txt')
 
 
-execute()
+main()
